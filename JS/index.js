cbe4bf7efa9960785f8321fb58dbff508d8106e3
JohnB08 <johnb08.kodehode@gmail.com>
2024-02-29 14:43:13 +0000

    init

---
+++ JS/index.js
@@ -0,0 +1,28 @@
+const btn = document.querySelector(".buttonGetBracket")
+const output = document.querySelector(".output")
+
+const fetchBrackets = async()=>{
+    const response = await fetch("http://localhost:3000/fetchBrackets")
+    const result = response.json()
+    return result
+}
+
+
+btn.addEventListener("click", async ()=>{
+    const brackets = await fetchBrackets()
+    console.log(brackets)
+    brackets.data.rows.forEach((bracket, i)=>{
+        const bracketDiv = document.createElement("div")
+        const bracketTitle = document.createElement("h2")
+        bracketTitle.innerText = `Bracket ${i+1}:`
+        bracketDiv.appendChild(bracketTitle)
+        bracketDiv.className = "bracket"
+        bracket.usernames.forEach(name=>{
+            const username = document.createElement("h3")
+            username.className = "name"
+            username.innerText = name
+            bracketDiv.append(username)
+        })
+        output.appendChild(bracketDiv)
+    })
+})
